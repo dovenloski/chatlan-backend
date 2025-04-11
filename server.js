c4f18fe9d477bc3970a9e3f37092d10e00b46067
dovenloski <dovenloski86@gmail.com>
2025-04-11 18:05:43 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-// server.js ChatLAN 2.9 reconstruido
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
+
+app.use(cors());
+app.use(express.json());
+
+io.on("connection", (socket) => {
+  socket.on("message", (data) => {
+    io.emit("message", data);
+  });
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Servidor ChatLAN corriendo...");
+});
